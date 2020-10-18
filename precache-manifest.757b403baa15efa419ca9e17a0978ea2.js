6aa55ae002b631f7ea1608079c34aebb0b13a793
Phyo <soewaiphyo.jelly@gmail.com>
2020-10-18 22:00:24 +0800

    Updates

---
+++ precache-manifest.757b403baa15efa419ca9e17a0978ea2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8ac2ec7a0995f8e46f153eb3376745",
+    "url": "/plant-book/index.html"
+  },
+  {
+    "revision": "c1175965eb39b439c65e",
+    "url": "/plant-book/static/css/main.a427e57c.chunk.css"
+  },
+  {
+    "revision": "805747b3aecc4143d9f8",
+    "url": "/plant-book/static/js/2.23f4dc26.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/plant-book/static/js/2.23f4dc26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1175965eb39b439c65e",
+    "url": "/plant-book/static/js/main.fd2a5bbd.chunk.js"
+  },
+  {
+    "revision": "ce9e6cee4869f7ba927c",
+    "url": "/plant-book/static/js/runtime-main.a257bdf1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/plant-book/static/media/logo.5d5d9eef.svg"
+  }
+]);
